8d0d6eb8e63870a359ad0f6374acbc18a75b8f91
Scott Schmidt <s@scottschmidt.io>
2022-09-05 22:36:26 -0400

    First commit with create-react-app and some tinkering

---
+++ frontend/src/components/Politicians.jsx
@@ -0,0 +1,62 @@
+import {useEffect, useState} from 'react';
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableRow,
+	Paper,
+} from '@material-ui/core';
+
+
+function politiciansTable(pidToNameMap) {
+	return (
+		<TableContainer component={Paper}>
+		  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+			<TableBody>
+			  {Object.entries(pidToNameMap).map(([pid, name]) => (
+				<TableRow
+				  key={pid}
+				  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+				>
+				  <TableCell component="th" scope="row">
+					<a href={`http://127.0.0.1:5000/by-pid/${pid}`}>{name}</a>
+				  </TableCell>
+				</TableRow>
+			  ))}
+			</TableBody>
+		  </Table>
+		</TableContainer>
+	);
+}
+
+function Politicians() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {    
+		fetch("http://127.0.0.1:5000/get-pids")
+			.then(res => res.json())
+			.then(
+			(result) => {
+				setIsLoaded(true);
+				setItems(result);
+			},
+			(error) => {
+				setIsLoaded(true);
+				setError(error);
+			}
+			)
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+	  return politiciansTable(items);
+	}
+  }
+
+export default Politicians;
