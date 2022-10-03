c9283e309cd4abf600adfe35864e061196691e66
Scott Schmidt <s@scottschmidt.io>
2022-10-02 23:10:47 -0400

    Complete /buy endpoint

--- backend/trade_api/server.go
+++ backend/trade_api/server.go
@@ -1,8 +1,29 @@
+// A web-server written in Go to handle certain requests to the Alpaca API.
+//
+// Main website:
+// 	https://alpaca.markets/
+//
+// API Documentation
+// 	https://alpaca.markets/deprecated/docs/api-documentation/api-v2/
+//
+// Must have a `.env` file in root directory with the following variables.
+// APCA_API_KEY_ID, APCA_API_SECRET_KEY, APCA_API_BASE_URL, PORT
+//
+// Execute the following to run the server.
+// $ go run trade_api/server.go
+//
+// Sample request
+// $ curl -X POST \
+//	-d \ '{"symbol": "AAPL", "qty": 1}' \
+// 	-H 'Content-Type: application/json' \
+//  localhost:5000/buy
+
 package main
 
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -10,7 +31,6 @@ import (
 	"github.com/alpacahq/alpaca-trade-api-go/v2/alpaca"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"github.com/shopspring/decimal"
 )
 
 func init() {
@@ -46,24 +66,40 @@ func getPositions(w http.ResponseWriter, _ *http.Request) {
 	json.NewEncoder(w).Encode(pos)
 }
 
-// Initiate the purchase of an asset.
+// Initiate the purchase of an asset with a POST request of the following shape:
+// 	{"symbol": string, "qty": decimal}
 func buy(w http.ResponseWriter, r *http.Request) {
-	var alpacaClient alpaca.Client = alpaca.NewClient(alpaca.ClientOpts{})
+	// Read the buy order from the POST request.
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
 
-	ticker := "AAPL"
-	qty := decimal.NewFromFloat(1)
-	resp, err := alpacaClient.PlaceOrder(
-		alpaca.PlaceOrderRequest{
-			AssetKey:    &ticker,
-			Qty:         &qty,
-			Side:        alpaca.Buy,
-			Type:        alpaca.Market,
-			TimeInForce: alpaca.GTC,
-		},
-	)
+	// Marshall the POST request JSON into the correct struct.
+	//
+	// Instantiated with required fields according to the API documentation.
+	// These defaults make sense for what we are building this for, so I don't think
+	// they should be API parameters.
+	//
+	// See `[POST] Request a new order` below.
+	// https://alpaca.markets/deprecated/docs/api-documentation/api-v2/orders/
+	buy_order := alpaca.PlaceOrderRequest{
+		Side:        alpaca.Buy,
+		Type:        alpaca.Market,
+		TimeInForce: alpaca.GTC,
+	}
+	if err := json.Unmarshal(body, &buy_order); err != nil {
+		panic(err)
+	}
+
+	// Place the buy order
+	var alpacaClient alpaca.Client = alpaca.NewClient(alpaca.ClientOpts{})
+	resp, err := alpacaClient.PlaceOrder(buy_order)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Return the buy order response as JSON
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(resp)
 }
